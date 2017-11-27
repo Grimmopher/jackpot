83ae58c5b47edce3e02b51170d7a6ab49864770f
Tara Grimm <tararochegrimm@gmail.com>
2017-11-27 13:40:03 -0500

    Added new media queries, styling for inputs. Checked for color vision accessibility.

--- src/NumberInput.js
+++ src/NumberInput.js
@@ -4,15 +4,26 @@ import 'react-day-picker/lib/style.css';
 
 export default (props) => {
     return (
-        <div>
-            <div>
-                {props.playerNumbers.map((n, i) => {
-                    return <input type="text" value={n} onChange={(event) => { props.onPlayerNumberChange(event, i) }} />
-                })}
+        <div className="number-input-container">
+            <div className="instructions">
+                Type in your Powerball lottery numbers below. <br/><span className="pb-instr">(Powerball number = orange)</span>
             </div>
+            <div className="player-nums">
+                {props.playerNumbers.map((n, i) => (i<5)
+                     ?  (
+                            <input className="num" type="text" value={n} onChange={(event) => { props.onPlayerNumberChange(event, i) }} />
+                        )
+                     :  (
+                               <input className="num pb" type="text" value={n} onChange={(event) => { props.onPlayerNumberChange(event, i) }} />
+                        )  
+                )}
+            </div>
+                <div className="instructions">
+                    Pick the date that you would like to see drawings from.
+                </div>
             <DayPickerInput onDayChange={props.onDayChange} />
             <div>
-                <button onClick={props.onClick} />
+                <button className="btn" onClick={props.onClick}>Submit</button>
             </div>
         </div>
     )
